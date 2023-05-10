c648bb668dbc474bf184895d75fc31f4698a2487
Qesait <kuzva2003@gmail.com>
2023-05-10 20:09:42 +0300

    ServerConnection implementation

--- src/data_providers/server_connection/ServerConnection.cpp
+++ src/data_providers/server_connection/ServerConnection.cpp
@@ -1,1 +1,28 @@
 #include "ServerConnection.h"
+
+#include <string>
+#include <iostream>
+
+#include <boost/asio.hpp>
+
+ServerConnection::ServerConnection(unsigned short port, const std::string &host)
+    : io_context_(), socket_(io_context_) {
+    boost::asio::ip::tcp::endpoint endpoint(
+        boost::asio::ip::address::from_string(host), port);
+    socket_.connect(endpoint);
+}
+
+ServerConnection::~ServerConnection() {
+    socket_.close();
+}
+
+std::string ServerConnection::request(const std::string &request) {
+    boost::asio::write(socket_, boost::asio::buffer(request));
+    //    buffer_.consume(buffer_.size())
+    size_t       bytes = boost::asio::read_until(socket_, buffer_, '\n');
+
+    std::string  response;
+    std::istream is(&buffer_);
+    std::getline(is, response);
+    return response;
+}
--- src/data_providers/server_connection/ServerConnection.h
+++ src/data_providers/server_connection/ServerConnection.h
@@ -1,6 +1,8 @@
 #ifndef DICT2FLASHCARDSQT_SRC_DATAPROVIDER_SERVERCONNECTION_SERVERCONNECTION_H
 #define DICT2FLASHCARDSQT_SRC_DATAPROVIDER_SERVERCONNECTION_SERVERCONNECTION_H
 
+#include <string>
+
 #include <boost/asio.hpp>
 
 #include "data_providers/irequestable/IRequestable.h"
@@ -9,14 +11,15 @@ using boost::asio::ip::tcp;
 
 class ServerConnection : public IRequestable {
  public:
-    ServerConnection(const std::string &port,
-                     const std::string &host = "localhost");
+    ServerConnection(unsigned short     port,
+                     const std::string &host = "127.0.0.1");
     ~ServerConnection();
     std::string request(const std::string &request) override;
 
  private:
     boost::asio::io_context io_context_;
     tcp::socket             socket_;
+    boost::asio::streambuf  buffer_;
 };
 
 #endif  // DICT2FLASHCARDSQT_SRC_DATAPROVIDER_SERVERCONNECTION_SERVERCONNECTION_H
