15b553cf1f2d494c97ae64f6426e3eab5add30aa
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-17 20:23:01 +0300

    feat(server/PluginsBundle): rewritten PluginsBuncdle with unique_ptr<>

--- server/lib/plugins/PluginsBundle/PluginsBundle.hpp
+++ server/lib/plugins/PluginsBundle/PluginsBundle.hpp
@@ -1,14 +1,15 @@
 #ifndef PLUGINS_BUNDLE_H
 #define PLUGINS_BUNDLE_H
 
-#include "AudiosProviderWrapper.hpp"
-#include "DefinitionsProviderWrapper.hpp"
-#include "FormatProcessorWrapper.hpp"
-#include "IPluginWrapper.hpp"
-#include "ImagesProviderWrapper.hpp"
-#include "SentencesProviderWrapper.hpp"
+#include "IAudiosProviderWrapper.hpp"
+#include "IDefinitionsProviderWrapper.hpp"
+#include "IFormatProcessorWrapper.hpp"
+#include "IImagesProviderWrapper.hpp"
+#include "ISentencesProviderWrapper.hpp"
+
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <optional>
 #include <string>
 #include <variant>
@@ -18,35 +19,40 @@ class PluginsBundle {
  public:
     PluginsBundle();
 
-    auto set_definitions_provider(DefinitionsProviderWrapper &&new_provider)
+    auto set_definitions_provider(
+        std::unique_ptr<IDefinitionsProviderWrapper> &&new_provider) -> void;
+
+    auto set_sentences_provider(
+        std::unique_ptr<ISentencesProviderWrapper> &&new_provider) -> void;
+
+    auto
+    set_images_provider(std::unique_ptr<IImagesProviderWrapper> &&new_provider)
         -> void;
 
-    auto set_sentences_provider(SentencesProviderWrapper &&new_provider)
+    auto
+    set_audios_provider(std::unique_ptr<IAudiosProviderWrapper> &&new_provider)
         -> void;
 
-    auto set_images_provider(ImagesProviderWrapper &&new_provider) -> void;
+    auto set_format_processor(
+        std::unique_ptr<IFormatProcessorWrapper> &&new_provider) -> void;
 
-    auto set_audios_provider(AudiosProviderWrapper &&new_provider) -> void;
+    auto definitions_provider() -> const IDefinitionsProviderWrapper *;
 
-    auto set_format_processor(FormatProcessorWrapper &&new_provider) -> void;
+    auto sentences_provider() -> const ISentencesProviderWrapper *;
 
-    auto definitions_provider() -> std::optional<DefinitionsProviderWrapper> &;
+    auto images_provider() -> const IImagesProviderWrapper *;
 
-    auto sentences_provider() -> std::optional<SentencesProviderWrapper> &;
+    auto audios_provider() -> const IAudiosProviderWrapper *;
 
-    auto images_provider() -> std::optional<ImagesProviderWrapper> &;
-
-    auto audios_provider() -> std::optional<AudiosProviderWrapper> &;
-
-    auto format_processor() -> std::optional<FormatProcessorWrapper> &;
+    auto format_processor() -> const IFormatProcessorWrapper *;
 
  private:
-    std::optional<DefinitionsProviderWrapper> definitions_provider_ =
-        std::nullopt;
-    std::optional<SentencesProviderWrapper> sentences_provider_ = std::nullopt;
-    std::optional<ImagesProviderWrapper>    images_provider_    = std::nullopt;
-    std::optional<AudiosProviderWrapper>    audios_provider_    = std::nullopt;
-    std::optional<FormatProcessorWrapper>   format_processor_   = std::nullopt;
+    std::unique_ptr<IDefinitionsProviderWrapper> definitions_provider_ =
+        nullptr;
+    std::unique_ptr<ISentencesProviderWrapper> sentences_provider_ = nullptr;
+    std::unique_ptr<IImagesProviderWrapper>    images_provider_    = nullptr;
+    std::unique_ptr<IAudiosProviderWrapper>    audios_provider_    = nullptr;
+    std::unique_ptr<IFormatProcessorWrapper>   format_processor_   = nullptr;
 };
 
 #endif  // !PLUGINS_BUNDLE_H
