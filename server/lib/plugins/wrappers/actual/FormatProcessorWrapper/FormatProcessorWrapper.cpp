1449a699e80f738007028e2b43017f87ce585eb7
Danila Slesarev <blackdeer1524@yandex.ru>
2023-05-20 02:33:07 +0300

    feat(server/plugins): untested format processor

--- server/lib/plugins/wrappers/actual/FormatProcessorWrapper/FormatProcessorWrapper.cpp
+++ server/lib/plugins/wrappers/actual/FormatProcessorWrapper/FormatProcessorWrapper.cpp
@@ -1,5 +1,6 @@
 #include "FormatProcessorWrapper.hpp"
 #include "BasePluginWrapper.hpp"
+#include "PyExceptionInfo.hpp"
 #include <boost/python/errors.hpp>
 #include <boost/python/extract.hpp>
 #include <boost/python/import.hpp>
@@ -54,10 +55,12 @@ auto FormatProcessorWrapper::build(const std::string           &name,
 
 auto FormatProcessorWrapper::save(const ResultFilesPaths &paths)
     -> std::variant<FormatProcessorWrapper::type, PyExceptionInfo> {
+    std::string res;
     try {
-        specifics_.save(
-            paths.cards.string(), paths.audios.string(), paths.images.string());
+        boost::python::object py_res = specifics_.save(paths.cards.string());
+        res = boost::python::extract<std::string>(py_res);
     } catch (const boost::python::error_already_set &) {
+        return PyExceptionInfo::build().value();
     }
-    return {};
+    return res;
 }
--- server/lib/plugins/wrappers/interfaces/IFormatProcessorWrapper/IFormatProcessorWrapper.hpp
+++ server/lib/plugins/wrappers/interfaces/IFormatProcessorWrapper/IFormatProcessorWrapper.hpp
@@ -4,13 +4,24 @@
 #include "IPluginWrapper.hpp"
 
 #include <filesystem>
+#include <nlohmann/detail/macro_scope.hpp>
 #include <string>
 
 struct ResultFilesPaths {
     std::filesystem::path cards;
-    std::filesystem::path audios;
-    std::filesystem::path images;
+    // std::filesystem::path audios;
+    // std::filesystem::path images;
 };
+
+inline void to_json(nlohmann ::json        &nlohmann_json_j,
+                    const ResultFilesPaths &nlohmann_json_t) {
+    nlohmann_json_j[0] = nlohmann_json_t.cards;
+}
+
+inline void from_json(const nlohmann ::json &nlohmann_json_j,
+                      ResultFilesPaths      &nlohmann_json_t) {
+    nlohmann_json_j[0].get_to(nlohmann_json_t.cards);
+}
 
 class IFormatProcessorWrapper : public virtual IPluginWrapper {
  public:
