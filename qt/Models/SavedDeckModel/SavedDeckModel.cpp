a46b6335a3ffc15b80032833536cc56839a5611e
Vladislav Budin <ladudin121@mail.ru>
2023-05-25 14:13:34 +0300

    Описал модель SavedDeckModel

--- qt/Models/SavedDeckModel/SavedDeckModel.cpp
+++ qt/Models/SavedDeckModel/SavedDeckModel.cpp
@@ -1,13 +1,27 @@
 #include "SavedDeckModel.hpp"
+#include <qabstractitemmodel.h>
+#include <qnamespace.h>
+#include <qvariant.h>
 
-SavedDeckModel::SavedDeckModel(QObject *parent)
+SavedDeckModel::SavedDeckModel(std::vector<Card> cards, QObject *parent)
     : QAbstractTableModel(parent)
+    , cards_(cards)
 {
 }
 
 QVariant SavedDeckModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    // FIXME: Implement me!
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (section) {
+            case 0:
+                return "word";
+            case 1:
+                return "definition";
+            default:
+            return QVariant();
+        }
+    }
+    return QVariant();
 }
 
 int SavedDeckModel::rowCount(const QModelIndex &parent) const
@@ -15,7 +29,7 @@ int SavedDeckModel::rowCount(const QModelIndex &parent) const
     if (parent.isValid())
         return 0;
 
-    // FIXME: Implement me!
+    return cards_.size();
 }
 
 int SavedDeckModel::columnCount(const QModelIndex &parent) const
@@ -23,7 +37,7 @@ int SavedDeckModel::columnCount(const QModelIndex &parent) const
     if (parent.isValid())
         return 0;
 
-    // FIXME: Implement me!
+    return 2;
 }
 
 QVariant SavedDeckModel::data(const QModelIndex &index, int role) const
@@ -31,20 +45,39 @@ QVariant SavedDeckModel::data(const QModelIndex &index, int role) const
     if (!index.isValid())
         return QVariant();
 
-    // FIXME: Implement me!
+    if (!hasIndex(index.row(), index.column())) {
+        return QVariant();
+    }
+    switch (role) {
+        case Qt::DisplayRole:
+            if (index.column() == 0) {
+                return QString::fromStdString(cards_.at(index.row()).word);
+            }
+            if (index.column() == 1) {
+                return QString::fromStdString(cards_.at(index.row()).definition);
+            }
+            return QVariant();
+        default:
+        return QVariant();
+    }
     return QVariant();
 }
 
 bool SavedDeckModel::removeRows(int row, int count, const QModelIndex &parent)
 {
     beginRemoveRows(parent, row, row + count - 1);
-    // FIXME: Implement me!
+    cards_.erase(cards_.begin() + row, cards_.begin() + row + count);
     endRemoveRows();
+    return true;
 }
 
-bool SavedDeckModel::removeColumns(int column, int count, const QModelIndex &parent)
-{
-    beginRemoveColumns(parent, column, column + count - 1);
-    // FIXME: Implement me!
-    endRemoveColumns();
+QModelIndex SavedDeckModel::next(const QModelIndex& index) {
+    // if (!hasIndex(index.row() + 1, index.column())) {
+    //     return index;
+    // }
+    // return createIndex(index.row() + 1, col)
+    return QModelIndex();
+}
+QModelIndex prev(const QModelIndex& index) {
+    return QModelIndex();
 }
--- qt/Models/SavedDeckModel/SavedDeckModel.hpp
+++ qt/Models/SavedDeckModel/SavedDeckModel.hpp
@@ -2,13 +2,15 @@
 #define SAVEDDECKMODEL_H
 
 #include <QAbstractTableModel>
+#include <vector>
+#include "Card.h"
 
 class SavedDeckModel : public QAbstractTableModel
 {
     Q_OBJECT
 
 public:
-    explicit SavedDeckModel(QObject *parent = nullptr);
+    explicit SavedDeckModel(std::vector<Card> cards, QObject *parent = nullptr);
 
     // Header:
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
@@ -18,12 +20,15 @@ public:
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
     // Remove data:
     bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
-    bool removeColumns(int column, int count, const QModelIndex &parent = QModelIndex()) override;
+
+public slots:
+    QModelIndex next(const QModelIndex& index);
+    QModelIndex prev(const QModelIndex& index);
 
 private:
+    std::vector<Card> cards_;
 };
 
 #endif // SAVEDDECKMODEL_H
